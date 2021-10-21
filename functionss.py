0307467fd7149f17def3097724ba297f07a2b770
elithaxxor <86808805+elithaxxor@users.noreply.github.com>
2021-10-21 16:08:05 -0700

    Update functionss.py

--- functionss.py
+++ functionss.py
@@ -1,3 +1,5 @@
+
+## RANDOM SHUFFLE 
 deck = 'ace two three four'.split()
 >>> shuffle(deck)                        # Shuffle a list
 >>> deck
@@ -5,3 +7,29 @@ deck = 'ace two three four'.split()
 
 >>> sample([10, 20, 30, 40, 50], k=4)    # Four samples without replacement
 [40, 10, 50, 30]
+
+
+
+## DAEMON THREAD 
+import Queue
+import threading
+
+def basic_worker(queue):
+    while True:
+        item = queue.get()
+        # do_work(item)
+        print(item)
+        queue.task_done()
+def basic():
+    # http://docs.python.org/library/queue.html
+    queue = Queue.Queue()
+    for i in range(3):
+         t = threading.Thread(target=basic_worker,args=(queue,))
+         t.daemon = True
+         t.start()
+    for item in range(4):
+        queue.put(item)
+    queue.join()       # block until all tasks are done
+    print('got here')
+
+basic()
