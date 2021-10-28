d99e42d8babff1b01bc1fa4bef39fee16469ae37
elithaxxor <86808805+elithaxxor@users.noreply.github.com>
2021-10-28 02:00:32 -0700

    Create pretty.py

---
+++ pretty.py
@@ -0,0 +1,139 @@
+
+
+class Colors:
+    reset = "\033[0m"
+
+    # Black
+    fgBlack = "\033[30m"
+    fgBrightBlack = "\033[30;1m"
+    bgBlack = "\033[40m"
+    bgBrightBlack = "\033[40;1m"
+
+    # Red
+    fgRed = "\033[31m"
+    fgBrightRed = "\033[31;1m"
+    bgRed = "\033[41m"
+    bgBrightRed = "\033[41;1m"
+
+    # Green
+    fgGreen = "\033[32m"
+    fgBrightGreen = "\033[32;1m"
+    bgGreen = "\033[42m"
+    bgBrightGreen = "\033[42;1m"
+
+    # Yellow
+    fgYellow = "\033[33m"
+    fgBrightYellow = "\033[33;1m"
+    bgYellow = "\033[43m"
+    bgBrightYellow = "\033[43;1m"
+
+    # Blue
+    fgBlue = "\033[34m"
+    fgBrightBlue = "\033[34;1m"
+    bgBlue = "\033[44m"
+    bgBrightBlue = "\033[44;1m"
+    # Magenta
+    fgMagenta = "\033[35m"
+    fgBrightMagenta = "\033[35;1m"
+    bgMagenta = "\033[45m"
+    bgBrightMagenta = "\033[45;1m"
+    # Cyan
+    fgCyan = "\033[36m"
+    fgBrightCyan = "\033[36;1m"
+    bgCyan = "\033[46m"
+    bgBrightCyan = "\033[46;1m"
+    # White
+    fgWhite = "\033[37m"
+    fgBrightWhite = "\033[37;1m"
+    bgWhite = "\033[47m"
+    bgBrightWhite = "\033[47;1m"
+
+
+###########
+color = Colors()
+spinner = Spinner()
+yellow = color.fgYellow
+red = color.fgRed
+blue = color.fgBlue
+bblue = color.fgBrightBlue
+cyan = color.fgCyan
+bg_background = color.bgBlack
+reset = color.reset
+
+
+############
+
+
+def period_wait():
+    period = ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.']
+    # multi = [2,2,2,2,2,2,2,2,2,2]
+    period_len = len(period)
+    with Spinner():
+        for z, x in enumerate(period):
+            print(x)
+            time.sleep(.2)
+            if z <= period_len:
+                z += 1
+                print(f"{yellow}{x * z}{reset}")
+                continue
+            elif z == period_len:
+                break
+                
+
+def display_header():
+    # print('*' * 75)
+    color_red = Colors()
+    global red0
+    red0 = color_red.fgRed
+    global reset0
+    reset0 = color_red.reset
+
+    x = 'x'
+    print(f"{'X' * 125:^70}")
+    print(f"{'X' * 125:^70}")
+    pretty = f'{red0}xxx FILE-MOVER xxx{reset0}'.center(width)
+    print(f'{pretty : ^70}')
+    print(f"{'X' * 125: ^70}")
+
+    one = (
+        f'[USAGE] - [1] This is a python program that takes a specified file names, and moves them into an individual folder.')
+    two = (
+        f'[USAGE] - [2] The program works well with most download repositories, and currently gets around security measure implimented by \n[USAGE] - [2]b 1337x.to, itorrent && archive.org')
+    three = (
+        f'[USAGE] - [3] Download a LINK GRABBING extension from chrome, to pull the URLs off of the browsers tabs.')
+    four = (f'[USAGE] - [4] Save the list into download_list.txt (Found in the Directory as this program')
+    five = (
+        f'[USAGE] - [5] Wait for downloads. Archive.org may be slow. The program saves both a LIST and DICT for further usage. (see functions)')
+    six = (f'[SYSTEM] copyright material from Adel Al-Aali [SYSTEM]')
+
+    print(f"{one:^70}")
+    print(f"{two:^70}")
+    print(f"{three:^70}")
+    print(f"{four:^70}")
+    print(f"{five:^70}")
+    print(f"{six:^70}")
+    print(f"{x * 20: ^70}")
+    print(), print()
+
+
+def period_wait():
+    period = ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.']
+    # multi = [2,2,2,2,2,2,2,2,2,2]
+    period_len = len(period)
+    with Spinner():
+        for z, x in enumerate(period):
+            print(x)
+            time.sleep(.2)
+            if z <= period_len:
+                z += 1
+                print(f"{yellow}{x * z}{reset}")
+                continue
+            elif z == period_len:
+                break
+
+
+def clear():
+    # check and make call for specific operating system
+    os_name = platform.system()
+    _ = call('clear' if os_name == 'Linux' or 'Windows' or 'Darwin' else 'cls')
+
